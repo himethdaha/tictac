896b40442d01725fe6985783038f242250319c5e
Himeth Dahanayake <himethdaha@gmail.com>
2022-07-01 13:35:05 -0400

    UI done

---
+++ plan.md
@@ -0,0 +1,14 @@
+## USER INTERFACE
+
+main header - tic-tac-toe (caps)
+BUTTONS - new game (hidden while game is played)
+Player 1 Score
+Player 2 Score
+3 \* 3 grid (no outer borders)
+
+## GAME LOGIC
+
+Player 1 is X
+Player 2 is O
+When a player clicks on a grid cell make it unclickable
+If a pattern of straight or diagonal is found end game and show winner
--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+function swimmer(name) {
+  return {
+    swim: () => {
+      console.log(`${name} swam`);
+    },
+  };
+}
 
+const obj = swimmer("Eagle");
+obj.swim();
+
+const alphabet = ["A", "B", "C", "D", "E"];
+const numbers = [1, 2, 3, 4, 5];
+
+const [a, , c, ...d] = alphabet;
+console.log(a, c, d);
+
+const conjoinedArray = [...alphabet, ...numbers];
+console.log(conjoinedArray);
+
+const objs = { a: 10, b: 90 };
+const { a: first } = objs;
+console.log(first);
+
+const personOne = {
+  name: "kyle",
+  age: 32,
+  favFood: "watermelon",
+  address: {
+    city: "Somewhere",
+    state: "ON",
+  },
+};
+
+const personTwo = {
+  name: "Sally",
+  age: 34,
+  favFood: "Rice",
+  address: {
+    city: "Somewhere Else",
+    state: "MN",
+  },
+};
+
+const personThree = { ...personOne, ...personTwo };
+console.log(personThree);
